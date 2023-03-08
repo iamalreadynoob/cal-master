6b35e396f6e350c5d6f81da6ff48a4abe80274a2
iamalreadynoob <22120205381@ismu.edu.tr>
2023-03-08 14:26:56 +0300

    commit

--- src/arithmetic/BasicArith.java
+++ src/arithmetic/BasicArith.java
@@ -9,220 +9,224 @@ public class BasicArith
     String answer;
     public BasicArith(String[] equation)
     {
-        ArrayList<String> symbols = new ArrayList<>();
-        ArrayList<Double> numbers = new ArrayList<>();
-        int remain;
-
-        if (equation[0].equals("-"))
+        if (equation != null)
         {
-            remain = 0;
-        }
+            ArrayList<String> symbols = new ArrayList<>();
+            ArrayList<Double> numbers = new ArrayList<>();
+            int remain;
 
-        else
-        {
-            symbols.add("+");
-            remain = 1;
-        }
+            if (equation[0].equals("-"))
+            {
+                remain = 0;
+            }
 
-        for (int i = 0; i < equation.length; i++)
-        {
-            if ((i+2) % 2 == remain) symbols.add(equation[i]);
             else
             {
-                try
-                {
-                    numbers.add(Double.parseDouble(equation[i]));
-                }
-                catch (Exception e)
-                {
-                    if (new Predefinitions().isConstant(equation[i]))
-                    {
-                        numbers.add(new Predefinitions().getConstants().get(equation[i]));
-                    }
-
-                    else if (equation[i].charAt(equation[i].length() - 1) == '!')
-                    {
-                        String temp = null;
-                        double number;
-
-                        for (int j = 0; j < equation[i].length()-1; j++)
-                        {
-                            if (temp == null) temp = Character.toString(equation[i].charAt(j));
-                            else temp += Character.toString(equation[i].charAt(j));
-                        }
-
-                        number = Double.parseDouble(temp);
-
-                        if (number >= 0)
-                        {
-                            if (number == 0) numbers.add((double)1);
-                            else
-                            {
-                                double tempResult = 1;
-
-                                for (int j = (int) number; j > 0; j--)
-                                {
-                                    tempResult *= j;
-                                }
-
-                                numbers.add(tempResult);
-                            }
-                        }
-                    }
-
-                    else if (equation[i].contains("^"))
-                    {
-                        Double exp = exponenter(equation[i]);
-
-                        if (exp != null) numbers.add(exp);
-                    }
-
-                    else if (equation[i].contains("#"))
-                    {
-                        int rootSymAmount = 0;
-                        int rootLoc = 0;
-                        for (int j = 0; j < equation[i].length(); j++)
-                        {
-                            if (equation[i].charAt(j) == '#')
-                            {
-                                rootSymAmount++;
-                                rootLoc = j;
-                            }
-
-                            if (rootSymAmount > 1) break;
-                        }
-
-                        if (rootSymAmount == 1)
-                        {
-                            if (rootLoc == 0)
-                            {
-                                String baseS = null;
-
-                                for (int j = rootLoc+1; j < equation[i].length(); j++)
-                                {
-                                    if (baseS == null) baseS = Character.toString(equation[i].charAt(j));
-                                    else baseS += Character.toString(equation[i].charAt(j));
-                                }
-
-                                numbers.add(Math.pow(Double.parseDouble(baseS), 0.5));
-                            }
-                            else if (rootLoc != equation[i].length()-1)
-                            {
-                                String powerS = null;
-                                String baseS = null;
-
-                                for (int j = 0; j < rootLoc; j++)
-                                {
-                                    if (powerS == null) powerS = Character.toString(equation[i].charAt(j));
-                                    else powerS += Character.toString(equation[i].charAt(j));
-                                }
-
-                                for (int j = rootLoc+1; j < equation[i].length(); j++)
-                                {
-                                    if (baseS == null) baseS = Character.toString(equation[i].charAt(j));
-                                    else baseS += Character.toString(equation[i].charAt(j));
-                                }
-
-                                numbers.add(Math.pow(Double.parseDouble(baseS), 1 / Double.parseDouble(powerS)));
-                            }
-                        }
-                    }
-
-                    else if (new Predefinitions().isTrig(equation[i]))
-                    {
-                        numbers.add(new Trigonometry(equation[i], Double.parseDouble(equation[i+1])).getResult());
-                        i++;
-                    }
-
-                }
+                symbols.add("+");
+                remain = 1;
             }
-        }
 
-        ArrayList<Double> remainedValues = numbers;
-        ArrayList<String> remainedSym = symbols;
-
-        boolean didChangeAnything = true;
-        while (didChangeAnything)
-        {
-            didChangeAnything = false;
-
-            for (int i = 0; i < remainedSym.size(); i++)
+            for (int i = 0; i < equation.length; i++)
             {
-                if (remainedSym.get(i).equals("*"))
+                if ((i+2) % 2 == remain) symbols.add(equation[i]);
+                else
                 {
-                    didChangeAnything = true;
-                    double temp = remainedValues.get(i-1) * remainedValues.get(i);
+                    try
+                    {
+                        numbers.add(Double.parseDouble(equation[i]));
+                    }
+                    catch (Exception e)
+                    {
+                        if (new Predefinitions().isConstant(equation[i]))
+                        {
+                            numbers.add(new Predefinitions().getConstants().get(equation[i]));
+                        }
 
-                    remainedValues.set(i-1, temp);
-                    remainedValues.remove(i);
-                    remainedSym.remove(i);
+                        else if (equation[i].charAt(equation[i].length() - 1) == '!')
+                        {
+                            String temp = null;
+                            double number;
 
-                }
+                            for (int j = 0; j < equation[i].length()-1; j++)
+                            {
+                                if (temp == null) temp = Character.toString(equation[i].charAt(j));
+                                else temp += Character.toString(equation[i].charAt(j));
+                            }
 
-                else if (remainedSym.get(i).equals("/"))
-                {
-                    didChangeAnything = true;
-                    double temp = remainedValues.get(i-1) / remainedValues.get(i);
+                            number = Double.parseDouble(temp);
 
-                    remainedValues.set(i-1, temp);
-                    remainedValues.remove(i);
-                    remainedSym.remove(i);
-                }
+                            if (number >= 0)
+                            {
+                                if (number == 0) numbers.add((double)1);
+                                else
+                                {
+                                    double tempResult = 1;
 
-                else if (remainedSym.get(i).equals("^"))
-                {
-                    didChangeAnything = true;
-                    double temp = Math.pow(remainedValues.get(i-1), remainedValues.get(i));
+                                    for (int j = (int) number; j > 0; j--)
+                                    {
+                                        tempResult *= j;
+                                    }
 
-                    remainedValues.set(i-1, temp);
-                    remainedValues.remove(i);
-                    remainedSym.remove(i);
-                }
+                                    numbers.add(tempResult);
+                                }
+                            }
+                        }
 
-                else if (remainedSym.get(i).equals("%"))
-                {
-                    didChangeAnything = true;
-                    double temp = remainedValues.get(i-1) % remainedValues.get(i);
+                        else if (equation[i].contains("^"))
+                        {
+                            Double exp = exponenter(equation[i]);
 
-                    remainedValues.set(i-1, temp);
-                    remainedValues.remove(i);
-                    remainedSym.remove(i);
-                }
+                            if (exp != null) numbers.add(exp);
+                        }
 
-                else if (remainedSym.get(i).equals("#"))
-                {
-                    didChangeAnything = true;
-                    double temp = Math.pow(remainedValues.get(i), 1 / remainedValues.get(i-1));
+                        else if (equation[i].contains("#"))
+                        {
+                            int rootSymAmount = 0;
+                            int rootLoc = 0;
+                            for (int j = 0; j < equation[i].length(); j++)
+                            {
+                                if (equation[i].charAt(j) == '#')
+                                {
+                                    rootSymAmount++;
+                                    rootLoc = j;
+                                }
 
-                    remainedValues.set(i-1, temp);
-                    remainedValues.remove(i);
-                    remainedSym.remove(i);
+                                if (rootSymAmount > 1) break;
+                            }
+
+                            if (rootSymAmount == 1)
+                            {
+                                if (rootLoc == 0)
+                                {
+                                    String baseS = null;
+
+                                    for (int j = rootLoc+1; j < equation[i].length(); j++)
+                                    {
+                                        if (baseS == null) baseS = Character.toString(equation[i].charAt(j));
+                                        else baseS += Character.toString(equation[i].charAt(j));
+                                    }
+
+                                    numbers.add(Math.pow(Double.parseDouble(baseS), 0.5));
+                                }
+                                else if (rootLoc != equation[i].length()-1)
+                                {
+                                    String powerS = null;
+                                    String baseS = null;
+
+                                    for (int j = 0; j < rootLoc; j++)
+                                    {
+                                        if (powerS == null) powerS = Character.toString(equation[i].charAt(j));
+                                        else powerS += Character.toString(equation[i].charAt(j));
+                                    }
+
+                                    for (int j = rootLoc+1; j < equation[i].length(); j++)
+                                    {
+                                        if (baseS == null) baseS = Character.toString(equation[i].charAt(j));
+                                        else baseS += Character.toString(equation[i].charAt(j));
+                                    }
+
+                                    numbers.add(Math.pow(Double.parseDouble(baseS), 1 / Double.parseDouble(powerS)));
+                                }
+                            }
+                        }
+
+                        else if (new Predefinitions().isTrig(equation[i]))
+                        {
+                            numbers.add(new Trigonometry(equation[i], Double.parseDouble(equation[i+1])).getResult());
+                            i++;
+                        }
+
+                    }
                 }
             }
-        }
 
-        double result;
+            ArrayList<Double> remainedValues = numbers;
+            ArrayList<String> remainedSym = symbols;
 
-        if (remainedSym.isEmpty())
-        {
-            result = remainedValues.get(0);
-        }
-        else
-        {
-            result = 0;
-
-            for (int i = 0; i < remainedValues.size(); i++)
+            boolean didChangeAnything = true;
+            while (didChangeAnything)
             {
-                if (remainedSym.get(i).equals("+")) result += remainedValues.get(i);
-                else if (remainedSym.get(i).equals("-")) result -= remainedValues.get(i);
+                didChangeAnything = false;
+
+                for (int i = 0; i < remainedSym.size(); i++)
+                {
+                    if (remainedSym.get(i).equals("*"))
+                    {
+                        didChangeAnything = true;
+                        double temp = remainedValues.get(i-1) * remainedValues.get(i);
+
+                        remainedValues.set(i-1, temp);
+                        remainedValues.remove(i);
+                        remainedSym.remove(i);
+
+                    }
+
+                    else if (remainedSym.get(i).equals("/"))
+                    {
+                        didChangeAnything = true;
+                        double temp = remainedValues.get(i-1) / remainedValues.get(i);
+
+                        remainedValues.set(i-1, temp);
+                        remainedValues.remove(i);
+                        remainedSym.remove(i);
+                    }
+
+                    else if (remainedSym.get(i).equals("^"))
+                    {
+                        didChangeAnything = true;
+                        double temp = Math.pow(remainedValues.get(i-1), remainedValues.get(i));
+
+                        remainedValues.set(i-1, temp);
+                        remainedValues.remove(i);
+                        remainedSym.remove(i);
+                    }
+
+                    else if (remainedSym.get(i).equals("%"))
+                    {
+                        didChangeAnything = true;
+                        double temp = remainedValues.get(i-1) % remainedValues.get(i);
+
+                        remainedValues.set(i-1, temp);
+                        remainedValues.remove(i);
+                        remainedSym.remove(i);
+                    }
+
+                    else if (remainedSym.get(i).equals("#"))
+                    {
+                        didChangeAnything = true;
+                        double temp = Math.pow(remainedValues.get(i), 1 / remainedValues.get(i-1));
+
+                        remainedValues.set(i-1, temp);
+                        remainedValues.remove(i);
+                        remainedSym.remove(i);
+                    }
+                }
             }
+
+            double result;
+
+            if (remainedSym.isEmpty())
+            {
+                result = remainedValues.get(0);
+            }
+            else
+            {
+                result = 0;
+
+                for (int i = 0; i < remainedValues.size(); i++)
+                {
+                    if (remainedSym.get(i).equals("+")) result += remainedValues.get(i);
+                    else if (remainedSym.get(i).equals("-")) result -= remainedValues.get(i);
+                }
+            }
+
+            if (result - (double)(Math.round(result)) == 0)
+            {
+                answer = Integer.toString((int) Math.round(result));
+            }
+            else answer = Double.toString(result);
         }
 
-        if (result - (double)(Math.round(result)) == 0)
-        {
-            answer = Integer.toString((int) Math.round(result));
-        }
-        else answer = Double.toString(result);
     }
 
     public String getAnswer() {return answer;}
@@ -265,5 +269,26 @@ public class BasicArith
         }catch (Exception e) {}
 
         return expVal;
+    }
+
+    public boolean isPrime(int number)
+    {
+        boolean isIt = true;
+
+        if (number <= 1) isIt = false;
+
+        else
+        {
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    isIt = false;
+                    break;
+                }
+            }
+        }
+
+        return isIt;
     }
 }
--- src/finder/PolynomialFind.java
+++ src/finder/PolynomialFind.java
@@ -2,6 +2,7 @@ package finder;
 
 import arithmetic.BasicArith;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 
 public class PolynomialFind
@@ -20,10 +21,16 @@ public class PolynomialFind
         simplyEquation(command);
         seperator();
 
-        System.out.println(equation);
-        System.out.println(sign);
-        System.out.println(coefficient);
-        System.out.println(valPower);
+        if (isSuitable())
+        {
+            double p = getP();
+            double q = getQ();
+            ArrayList<Integer> factorizedP = factorize((int) Math.abs(p));
+            ArrayList<Integer> factorizedQ = factorize((int) Math.abs(q));
+
+            System.out.println(factorizedP);
+            System.out.println(factorizedQ);
+        }
     }
 
     protected String getAnswer() {return answer;}
@@ -31,10 +38,7 @@ public class PolynomialFind
     private void simplyEquation(String[] command)
     {
 
-        if (!(command[3].equals("+") || command[3].equals("-")))
-        {
-            equation.add("+");
-        }
+        if (!(command[3].equals("+") || command[3].equals("-"))) equation.add("+");
 
         Integer otherSide = null;
         for (int i = 3; i < command.length; i++)
@@ -66,6 +70,9 @@ public class PolynomialFind
     {
         int location = 0;
 
+        for (int i = 0; i < equation.size(); i++) if (equation.get(i).equals(respectedBy)) coefficient.add("1");
+        int varLocation = 0;
+
         if (!(equation.get(location).equals("+") || equation.get(location).equals("-")))
         {
             sign.add("+");
@@ -85,13 +92,21 @@ public class PolynomialFind
                 location++;
             }
 
-            if (!(equation.get(location).equals("x") && equation.get(location).equals("+")
-            && equation.get(location).equals("-")))
+            if (equation.get(location).equals(respectedBy)) varLocation++;
+
+            if (!(equation.get(location).equals(respectedBy) || equation.get(location).equals("+")
+            || equation.get(location).equals("-")))
             {
                 ArrayList<String> coefficients = new ArrayList<>();
                 boolean isCoefficient = true;
 
-                while (true)
+                if (location - 1 > 0 && equation.get(location-1).equals("^"))
+                {
+                    isCoefficient = false;
+                    location++;
+                }
+
+                while (isCoefficient)
                 {
                     coefficients.add(equation.get(location));
                     location++;
@@ -109,16 +124,33 @@ public class PolynomialFind
                         break;
                     }
                 }
+                System.out.println(coefficients);
 
-                String[] coefArr = new String[coefficients.size()];
-                for (int i = 0; i < coefficients.size(); i++) coefArr[i] = coefficients.get(i);
-                String value = new BasicArith(coefArr).getAnswer();
+                if (!coefficients.isEmpty())
+                {
+                    String[] coefArr = new String[coefficients.size()];
+                    for (int i = 0; i < coefficients.size(); i++) coefArr[i] = coefficients.get(i);
+                    String value = new BasicArith(coefArr).getAnswer();
 
-                coefficient.add(value);
-                if (!isCoefficient) valPower.add("0");
+
+                    if (isCoefficient)
+                    {
+                        coefficient.set(varLocation, value);
+                    }
+                    else
+                    {
+                        valPower.add("0");
+                        coefficient.add("1");
+
+                        for (int i = coefficients.size()-2; i >= varLocation; i--) coefficient.set(i+1, coefficient.get(i));
+                        coefficient.set(varLocation, value);
+                    }
+
+                    varLocation++;
+                }
             }
 
-            if (equation.get(location).equals("x"))
+            if (location < equation.size() && equation.get(location).equals("x"))
             {
                 location++;
                 if (location < equation.size())
@@ -134,6 +166,111 @@ public class PolynomialFind
             }
 
         }
+    }
+
+    private boolean isSuitable()
+    {
+        boolean isIt = true;
+
+        for (int i = 0; i < coefficient.size(); i++)
+        {
+            if (Double.parseDouble(coefficient.get(i)) != (int)Double.parseDouble(coefficient.get(i)))
+            {
+                double floating = Double.parseDouble(coefficient.get(i)) - (int) Double.parseDouble(coefficient.get(i));
+                double required = 1 / floating;
+
+                if (required == (int) required)
+                {
+                    for (int j = 0; j < coefficient.size(); j++)
+                    {
+                        double currentVal = Double.parseDouble(coefficient.get(j));
+                        coefficient.set(j, Double.toString(currentVal * required));
+                    }
+                }
+                else
+                {
+                    isIt = false;
+                    break;
+                }
+            }
+        }
+
+        return isIt;
+    }
+
+    private double getP()
+    {
+        double p = 0;
+
+        for (int i = 0; i < valPower.size(); i++)
+        {
+            if (valPower.get(i).equals("0"))
+            {
+                if (sign.get(i).equals("+")) p += Double.parseDouble(coefficient.get(i));
+                else p -= Double.parseDouble(coefficient.get(i));
+            }
+        }
+
+        return p;
+    }
+
+    private double getQ()
+    {
+        int highestPow = 0;
+        double q = 0;
+
+        for (int i = 0; i < valPower.size(); i++)
+        {
+            if (highestPow < Integer.parseInt(valPower.get(i))) highestPow = Integer.parseInt(valPower.get(i));
+        }
+
+        for (int i = 0; i < valPower.size(); i++)
+        {
+            if (valPower.get(i).equals(Integer.toString(highestPow)))
+            {
+                if (sign.get(i).equals("+")) q += Double.parseDouble(coefficient.get(i));
+                else q -= Double.parseDouble(coefficient.get(i));
+            }
+        }
+
+        return q;
+    }
+
+    private ArrayList<Integer> factorize(int number)
+    {
+        // TODO: Find the problem
+        ArrayList<Integer> factorized = new ArrayList<>();
+        double value = number;
+        double divideBy = number;
+
+        factorized.add(1);
+        while (value != 1 && divideBy >= 1)
+        {
+            if (new BasicArith(null).isPrime((int) divideBy))
+            {
+                if (value / divideBy == (int)(value / divideBy))
+                {
+                    boolean isExist = false;
+
+                    for (int i = 0; i < factorized.size(); i++)
+                    {
+                        if (factorized.get(i) == value / divideBy)
+                        {
+                            isExist = true;
+                            break;
+                        }
+                    }
+
+                    if (!isExist) factorized.add((int) divideBy);
+
+                    value /= divideBy;
+                }
+            }
+
+            divideBy--;
+        }
+
+        return factorized;
     }
 
 }
